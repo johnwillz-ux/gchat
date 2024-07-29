12ab3d17fdb6168bafda118b37c867289f171d0a
johnwillz-ux <johnwillz.ux@gmail.com>
2024-07-29 15:01:48 +0100

    chore: Add initial project structure and dependencies
    - Created directories for models, views, constants, animation, and repositories
    - Added dependencies for provider, firebase_database, http, and localstorage
    - Set up initial project configuration

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:g_chat/views/onboard_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const OnboardView(),
+    );
+  }
+}
